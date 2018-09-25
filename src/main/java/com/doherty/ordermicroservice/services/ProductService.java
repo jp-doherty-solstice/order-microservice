40c6d200a391a1f8d62918362e304a63e3d2bc24
John Doherty <jpdoherty@solstice.com>
2018-09-25 14:57:23 -0500

    changed name of feign service call

--- src/main/java/com/doherty/ordermicroservice/services/ProductService.java
+++ src/main/java/com/doherty/ordermicroservice/services/ProductService.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "product-service")
+@FeignClient(name = "product-microservice")
 public interface ProductService {
 
     @RequestMapping(value = "/product/{id}/name", method = RequestMethod.GET)
